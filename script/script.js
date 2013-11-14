484caef3ad8302d5b7e48e38fd80df45be90b411
Nick Barry <nick@picturesandtext.co.uk>
2013-11-13 19:25:03 -0800

    Initial Commit

---
+++ script/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+  $('.tags').on('keyup', function () {
+    var self = $(this),
+        val = self.val(),
+        lastChar = val.slice(-1),
+        tag;
+
+    if (lastChar == ' ') {
+        val = val.trim();
+        if (val.charAt(0) != '#') {
+            val = '#' + val;
+        }
+        tag = $('<span class="tag"><span class="close">x</span>' + val + '</span>');
+        $('.tag-container').append(tag);
+        tag.animate({
+            'margin-left': 0
+        }, 60);
+
+        if (tag.is(':first-child'))
+            $('.submit').removeClass('disabled');
+        self.attr('placeholder', 'Suggest another tag, or press submit when you\'re done').val('').focus();
+    }
+}).on('keydown', function (e) {
+    if (e.keyCode == 13) {
+        e.preventDefault();
+    }
+});
+
+
+
+$(document).on('click', '.close', function () {
+    $(this).closest('.tag').remove();
+    if ($('.tag').length == 0) {
+        $('.submit').addClass('disabled');
+        $('.tags').attr('placeholder', 'Suggest a hashtag, ex. #midtownsac')
+    }
+});  
+})
