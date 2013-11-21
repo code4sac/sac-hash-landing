95cca2a2bbc3c24f4ce686cd62990725297fc1f4
Kaleb Clark <kaleb@abraxxus.net>
2013-11-20 20:38:10 -0800

    Added timestamp to table for suggestions

--- suggest.php
+++ suggest.php
@@ -6,7 +6,7 @@ $ht = filter_var($_POST['hashtag'], FILTER_SANITIZE_STRING);
 
 $query = "
   INSERT INTO hashtag_suggestions
-  VALUES('0', '$ht', '')
+  VALUES('0', '$ht', '', now())
 ";
 
 $mysql->insert($query);
